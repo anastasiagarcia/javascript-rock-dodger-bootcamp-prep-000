a37f8bab7223da84148556a16bbf813eef8c1617
Anastasia Pankina <anastaciap@ymail.com>
2019-08-25 13:36:06 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -161,6 +161,12 @@ function moveDodger(e) {
    * we've declared for you above.)
    * And be sure to use the functions declared below!
    */
+  if (e.which === LEFT_ARROW) {
+    moveDodgerLeft();
+  }
+  if (e.which === RIGHT_ARROW) {
+    moveDodgerRight();
+  }
 }
 
 function moveDodgerLeft() {
