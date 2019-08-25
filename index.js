a8b3b59cd40b76eba7ff16b1b73488d188a703f5
Anastasia Pankina <anastaciap@ymail.com>
2019-08-25 12:33:16 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ function checkCollision(rock) {
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
     const rockRightEdge = rockLeftEdge + 20;
-    if (rockLeftEdge < dodgerLeftEdge && rockRightEdge < dodgerRightEdge) {
+    if (rockLeftEdge < dodgerLeftEdge && rockRightEdge > dodgerRightEdge) {
       return true;
     }
     if (rockLeftEdge < dodgerRightEdge && rockLeftEdge > dodgerLeftEdge) {
