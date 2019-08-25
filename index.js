95347d34a43f949ecebb74e979b3b3d2bd708786
Anastasia Pankina <anastaciap@ymail.com>
2019-08-25 12:26:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -20,22 +20,35 @@ var gameInterval = null
 function checkCollision(rock) {
   // implement me!
   // use the comments below to guide you!
-  const top = positionToInteger(rock.style.top)
+
+  const topRock = positionToInteger(rock.style.top);
+  const bottomRock = topRock - 20;
+  const topDodger = positionToInteger(DODGER.style.top);
+  const bottomDodger = topDodger - 20;
+
 
   // rocks are 20px high
   // DODGER is 20px high
   // GAME_HEIGHT - 20 - 20 = 360px;
-  if (top > 360) {
+  if (topRock > 360) {
     const dodgerLeftEdge = positionToInteger(DODGER.style.left)
 
     // FIXME: The DODGER is 40 pixels wide -- how do we get the right edge?
-    const dodgerRightEdge = 0;
+    const dodgerRightEdge = dodgerLeftEdge + 40;
 
     const rockLeftEdge = positionToInteger(rock.style.left)
 
     // FIXME: The rock is 20 pixel's wide -- how do we get the right edge?
-    const rockRightEdge = 0;
-
+    const rockRightEdge = rockLeftEdge + 20;
+    if (rockLeftEdge < dodgerLeftEdge && rockRightEdge < dodgerRightEdge) {
+      return true;
+    }
+    if (rockLeftEdge < dodgerRightEdge && rockLeftEdge > dodgerLeftEdge) {
+      return true;
+    }
+    if (rockLeftEdge > dodgerLeftEdge && rockRightEdge < dodgerRightEdge) {
+      return true;
+    }
     if (false /**
                * Think about it -- what's happening here?
                * There's been a collision if one of three things is true:
@@ -150,7 +163,7 @@ function start() {
 
   START.style.display = 'none'
 
-  gameInterval = setInterval(function() {
-    createRock(Math.floor(Math.random() *  (GAME_WIDTH - 20)))
+  gameInterval = setInterval(function () {
+    createRock(Math.floor(Math.random() * (GAME_WIDTH - 20)))
   }, 1000)
 }
